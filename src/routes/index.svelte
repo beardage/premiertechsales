bd089f252083328a2be72292a7ea7f0274aed51a
Kyle Beard <xypress@gmail.com>
2022-01-22 23:02:58 -0700

    adding some default content for homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Premier Technical Sales</h1>
+
+<p>
+	Premier Tech Sales is a complete solution to your Field Representative needs in the states of
+	Arizona and New Mexico. We are focused on RF/Microwave and Digital products and take great pride
+	in offering professional services, working with our customers to support both existing and
+	cutting edge technologies.
+</p>
