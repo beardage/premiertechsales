d769386e07f4e831987d9129cdd30cb7cb79f4aa
Kyle Beard <xypress@gmail.com>
2022-01-23 14:58:58 -0700

    adding swiper and basic slider example

---
+++ src/lib/Slider/index.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { Swiper, SwiperSlide } from 'swiper/svelte';
+	import { EffectFade } from 'swiper';
+
+	import 'swiper/css';
+	import 'swiper/css/effect-fade';
+	import 'swiper/css/navigation';
+	import 'swiper/css/pagination';
+</script>
+
+<Swiper navigation pagination modules={[EffectFade]} effect="fade">
+	<SwiperSlide>Slide 1</SwiperSlide>
+	<SwiperSlide>Slide 2</SwiperSlide>
+	<SwiperSlide>Slide 3</SwiperSlide>
+</Swiper>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,9 @@
-<h1>Premier Technical Sales</h1>
+<script>
+	import Slider from '$lib/Slider/index.svelte';
+</script>
 
+<Slider />
+<h1>Premier Technical Sales</h1>
 <p>
 	Premier Tech Sales is a complete solution to your Field Representative needs in the states of
 	Arizona and New Mexico. We are focused on RF/Microwave and Digital products and take great pride
