74bfb5f5c9b5f7360b78095002b137ad8a970590
beardage <xypress@gmail.com>
2022-01-24 23:10:36 -0700

    adding routes for additional pages with demo content

---
+++ src/routes/about.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Slider from '$lib/Slider/index.svelte';
+</script>
+
+<Slider />
+<h1>Premier Technical Sales</h1>
+<p>
+	Premier Tech Sales is a complete solution to your Field Representative needs in the states of
+	Arizona and New Mexico. We are focused on RF/Microwave and Digital products and take great pride
+	in offering professional services, working with our customers to support both existing and
+	cutting edge technologies.
+</p>
