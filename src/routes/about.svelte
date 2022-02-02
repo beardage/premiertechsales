594a754df85fe8f1edea3827cd53abb5c5716a49
beardage <xypress@gmail.com>
2022-02-02 00:12:07 -0700

    adding content for about page and basic formatting

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,12 +1,60 @@
 <script>
-	import Slider from '$lib/Slider/index.svelte';
 </script>
 
-<Slider />
-<h1>Premier Technical Sales</h1>
+<h1 class="text-secondary-800 text-center text-4xl font-serif my-16">
+	About Premier Technical Sales
+</h1>
 <p>
-	Premier Tech Sales is a complete solution to your Field Representative needs in the states of
-	Arizona and New Mexico. We are focused on RF/Microwave and Digital products and take great pride
-	in offering professional services, working with our customers to support both existing and
-	cutting edge technologies.
+	Premier Technical Sales is one of the leading technical Field Representatives in the South West.
+	Originally founded under Youngewirth and Olenick, we have been servicing Commercial, Medical,
+	Military and Space customers in the AZ/NM territory for over 21 years. As you will see, the
+	companies we represent are all complimentary with each other, which is one of the keys to our
+	success. This allows for incredible lead generation, as well as being laser focused on the
+	entire system block diagram and not just one portion. We pride ourselves on not only having in
+	depth knowledge of the products we represent, but our customer needs as well. Premier works with
+	our Principals and OEM Engineers from conceptual designs through production. We have worked very
+	hard to build up the reputation of being one of the best in our fields and thus how we came up
+	with the name PREMIER.
 </p>
+
+<div
+	class="container mx-auto mt-8 md:mt-16 px-8 md:px-16 py-8 md:flex md:justify-center md:items-center"
+>
+	<div class="md:w-2/5 p-8">
+		<picture>
+			<img src="/premier-jeff.jpg" alt="Jeff Beard" />
+		</picture>
+	</div>
+	<div class="md:w-3/5 p-8 text-left">
+		<h2 class="font-bold text-xl mb-4">Jeff Beard, <em>CEO</em></h2>
+		<p>
+			I started my career in RF/Microwave in 1984, while working with Integrated Microwave
+			(IMC) as a Engineer Technician. After three years, I moved into Sales Manager roll (with
+			IMC) and managed all of the Reps nation-wide until 1990. I was then recruited by Piezo
+			Technology to be their Director of Sales for the Western Region, where again I managed
+			Reps for an additional 4 years and successfully grew our business. Opened Y&O AZ
+			(covering AZ/NM) in 1994 to Jan 1st of 2016. I have now purchased the AZ/NM territory
+			from Y&O, while still maintaining the same list of Principals.
+		</p>
+	</div>
+</div>
+
+<div
+	class="container mx-auto mt-8 md:mt-16 px-8 md:px-16 py-8 md:flex md:justify-center md:items-center"
+>
+	<div class="md:w-3/5 p-8 text-left">
+		<h2 class="font-bold text-xl mb-4">Amanda Rockrich</h2>
+		<p>
+			I have spent 9 years doing customer service related work in a broad capacity. I started
+			going to school for Nursing, then quickly realized that’s not what I wanted to pursue. I
+			then met Jeff and was very interested in hearing what he did for work. I am now proud to
+			say I work for Jeff Beard and I’m excited to continue my professional career working for
+			Premier Technical Sales extending my knowledge and growth in the field.
+		</p>
+	</div>
+	<div class="md:w-2/5 p-8">
+		<picture>
+			<img src="/premier-amanda.jpg" alt="Amanda Rockrich" />
+		</picture>
+	</div>
+</div>
