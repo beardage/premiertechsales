62f8c0a20349f1f1372e4320d547e5c6f06fc64e
beardage <xypress@gmail.com>
2022-02-12 14:42:45 -0700

    starting on linecard content

---
+++ src/routes/line-card.svelte
@@ -0,0 +1,20 @@
+<h1 class="text-secondary-800 text-center text-4xl font-serif my-16">Line Card</h1>
+
+<div class="flex">
+	<figure>
+		<a class="inline-block text-center" href="https://www.aethercomm.com/" target="_blank">
+			<picture>
+				<img src="/line-card/aethercomm.png" alt="aethercomm logo" />
+			</picture>
+		</a>
+		<figcaption>Aethercomm</figcaption>
+	</figure>
+	<figure>
+		<a class="inline-block text-center" href="https://www.aethercomm.com/" target="_blank">
+			<picture>
+				<img src="/line-card/crane.png" alt="crane logo" />
+			</picture>
+		</a>
+		<figcaption>Crane Aerospace & Electronics</figcaption>
+	</figure>
+</div>
