5d1e9518cd3015f2952d56df32ca007496f2cf01
kylebeardcc <kyle.beard@catchco.com>
2022-02-13 16:48:43 -0700

    adding couple more companies to line card

--- src/routes/line-card.svelte
+++ src/routes/line-card.svelte
@@ -10,11 +10,27 @@
 		<figcaption>Aethercomm</figcaption>
 	</figure>
 	<figure>
-		<a class="inline-block text-center" href="https://www.aethercomm.com/" target="_blank">
+		<a class="inline-block text-center" href="https://www.craneae.com/" target="_blank">
 			<picture>
 				<img src="/line-card/crane.png" alt="crane logo" />
 			</picture>
 		</a>
 		<figcaption>Crane Aerospace & Electronics</figcaption>
+	</figure>
+	<figure>
+		<a class="inline-block text-center" href="https://www.integratech.com/" target="_blank">
+			<picture>
+				<img src="/line-card/integra.png" alt="integra logo" />
+			</picture>
+		</a>
+		<figcaption>Integra RF Power Devices</figcaption>
+	</figure>
+	<figure>
+		<a class="inline-block text-center" href="https://www.kcbsolutions.com/" target="_blank">
+			<picture>
+				<img src="/line-card/kcb-solutions.png" alt="crane logo" />
+			</picture>
+		</a>
+		<figcaption>KCB Solutions</figcaption>
 	</figure>
 </div>
