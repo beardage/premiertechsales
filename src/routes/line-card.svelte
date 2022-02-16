f68714a76d26eb883c6bde2735a2b75a0064255e
beardage <xypress@gmail.com>
2022-02-15 23:46:00 -0700

    finishing up markup for line card

--- src/routes/line-card.svelte
+++ src/routes/line-card.svelte
@@ -1,7 +1,7 @@
 <h1 class="text-secondary-800 text-center text-4xl font-serif my-16">Line Card</h1>
 
-<div class="flex">
-	<figure>
+<div class="flex flex-wrap text-center">
+<figure class="basis-1/3">
 		<a class="inline-block text-center" href="https://www.aethercomm.com/" target="_blank">
 			<picture>
 				<img src="/line-card/aethercomm.png" alt="aethercomm logo" />
@@ -9,7 +9,7 @@
 		</a>
 		<figcaption>Aethercomm</figcaption>
 	</figure>
-	<figure>
+	<figure class="basis-1/3">
 		<a class="inline-block text-center" href="https://www.craneae.com/" target="_blank">
 			<picture>
 				<img src="/line-card/crane.png" alt="crane logo" />
@@ -17,7 +17,7 @@
 		</a>
 		<figcaption>Crane Aerospace & Electronics</figcaption>
 	</figure>
-	<figure>
+	<figure class="basis-1/3">
 		<a class="inline-block text-center" href="https://www.integratech.com/" target="_blank">
 			<picture>
 				<img src="/line-card/integra.png" alt="integra logo" />
@@ -25,12 +25,68 @@
 		</a>
 		<figcaption>Integra RF Power Devices</figcaption>
 	</figure>
-	<figure>
+	<figure class="basis-1/3">
 		<a class="inline-block text-center" href="https://www.kcbsolutions.com/" target="_blank">
 			<picture>
 				<img src="/line-card/kcb-solutions.png" alt="crane logo" />
 			</picture>
 		</a>
 		<figcaption>KCB Solutions</figcaption>
+	</figure>
+	<figure class="basis-1/3">
+		<a class="inline-block text-center" href="https://www.l3harris.com/" target="_blank">
+			<picture>
+				<img src="/line-card/l3harris.png" alt="l3harris logo" />
+			</picture>
+		</a>
+		<figcaption>L3Harris</figcaption>
+	</figure>
+	<figure class="basis-1/3">
+		<a class="inline-block text-center" href="https://www.markimicrowave.com/" target="_blank">
+			<picture>
+				<img src="/line-card/marki-microwave.jpg" alt="Marki Microwave logo" />
+			</picture>
+		</a>
+		<figcaption>Marki Microwave</figcaption>
+	</figure>
+	<figure class="basis-1/3">
+		<a class="inline-block text-center" href="https://www.mini-systemsinc.com/" target="_blank">
+			<picture>
+				<img src="/line-card/msi.png" alt="msi logo" />
+			</picture>
+		</a>
+		<figcaption>Mini-Systems Inc.</figcaption>
+	</figure>
+	<figure class="basis-1/3">
+		<a class="inline-block text-center" href="https://www.mtronpti.com/" target="_blank">
+			<picture>
+				<img src="/line-card/mtronPTI.png" alt="mtronpti logo" />
+			</picture>
+		</a>
+		<figcaption>MtronPTI</figcaption>
+	</figure>
+	<figure class="basis-1/3">
+		<a class="inline-block text-center" href="https://www.skyworksinc.com/" target="_blank">
+			<picture>
+				<img src="/line-card/skyworks.png" alt="skyworks logo" />
+			</picture>
+		</a>
+		<figcaption>Skyworks</figcaption>
+	</figure>
+	<figure class="basis-1/3">
+		<a class="inline-block text-center" href="https://www.timesmicrowave.com/" target="_blank">
+			<picture>
+				<img src="/line-card/timesmicrowave.png" alt="timesmicrowave logo" />
+			</picture>
+		</a>
+		<figcaption>Times Microwave Systems</figcaption>
+	</figure>
+	<figure class="basis-1/3">
+		<a class="inline-block text-center" href="https://www.skyworksinc.com/en/products/technical-ceramics" target="_blank">
+			<picture>
+				<img src="/line-card/trans-tech.png" alt="trans-tech logo" />
+			</picture>
+		</a>
+		<figcaption>Trans-Tech Ceramics</figcaption>
 	</figure>
 </div>
