3358537d9740cba5932d4e85c62794806831338c
kylebeardcc <kyle.beard@catchco.com>
2022-02-21 14:11:14 -0700

    adding page transition

---
+++ src/lib/Transitions/PageTransition.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { fly, fade } from 'svelte/transition';
+	export let url = '';
+</script>
+
+{#key url}
+	<div in:fly={{ delay: 100, duration: 200, y: -10 }} out:fly={{ duration: 100, y: 10 }}>
+		<slot />
+	</div>
+{/key}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,25 @@
+<script context="module">
+	export const load = async ({ url }) => ({
+		props: { url }
+	});
+</script>
+
 <script>
 	import '../styles/tailwind-output.css';
 	import Header from '$lib/Header/index.svelte';
 	import Footer from '$lib/Footer/index.svelte';
+	import PageTransition from '$lib/Transitions/PageTransition.svelte';
+	export let url;
 </script>
 
 <Header />
 
 <main>
-	<div class=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-16">
-		<slot />
-	</div>
+	<PageTransition {url}>
+		<div class=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-16">
+			<slot />
+		</div>
+	</PageTransition>
 </main>
 
 <Footer />
