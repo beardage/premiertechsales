fcea309d5af8e32e8a9862371bfd7672c5f905c0
beardage <xypress@gmail.com>
2022-02-26 10:48:11 -0700

    adding tailwind colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,17 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.gray,
+      green: colors.green,
+      cyan: colors.cyan,
+      primary: colors.green,
+      secondary: colors.cyan 
+    },
     extend: {
       fontFamily: {
         'headline': ['Roboto Condensed', 'sans-serif'],
