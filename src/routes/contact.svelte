c80fc54f2c7698e27d51ec12705d5b154a4dc8bf
beardage <xypress@gmail.com>
2022-02-26 10:48:22 -0700

    adding netlify contact form

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,22 +1,27 @@
 <script>
-	import Slider from '$lib/Slider/index.svelte';
-	import { compute_slots } from 'svelte/internal';
+	import Contact from "$lib/Forms/Contact.svelte";
 </script>
 
 <h1 class="text-secondary-800 text-center text-4xl font-headline my-16">Contact</h1>
 <div class="grid md:grid-cols-2 gap-8 mt-16">
-	<div class="order-2 md:order-1 my-4">
-		<p class="mb-4">
+	<div class="order-1 md:order-2 mb-4">
+		<h4 class="font-headline text-2xl">Office</h4>
+		<p class="mb-6">
 			Office Phone: <a href="tel:123.123.1234">123.123.1234</a><br />
 			Office Fax: <a href="fax:321.321.4321">321.321.4321</a><br />
 			Office Email:
 			<a href="mailto:emmccullough@premiertechsales.com">emccullough@premiertechsales.com</a>
 		</p>
-		<h4 class="font-bold text-xl">Jeff Beard</h4>
+		<h4 class="font-headline text-2xl">Jeff Beard</h4>
 		<p>
 			Phone: <a href="tel:602.799.8904">602.799.8904</a>
 			<br />
 			Email: <a href="mailto:jbeard@premiertechsales.com">jbeard@premiertechsales.com</a>
 		</p>
+	</div>
+	<div class="order-2 md: order-1">
+		<h2 class="text-2xl font-headline mb-2">Have an Inquiry?</h2>
+		<p>Send us a message and we'll get back to you as soon as possible</p>
+		<Contact/>
 	</div>
 </div>
