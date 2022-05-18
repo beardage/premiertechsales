26ddd59e414fffc0dfb0a3bd8c2598268b19377f
beardage <xypress@gmail.com>
2022-05-17 23:51:51 -0700

    matching the slider content styles better

--- src/lib/Slider/index.svelte
+++ src/lib/Slider/index.svelte
@@ -18,9 +18,9 @@
 			alt="military"
 			src="slider/pts-apache-2k.jpg"
 			/>
-		<div class="slide-content absolute bottom-0 left-0 p-4 bg-primary-800 text-gray-100">
-			<h2 class="text-2xl mb-2">Military</h2>
-			<p>A wide range of products supporting our troops from Communications to Defense.</p>
+		<div class="slide-content absolute bottom-8 left-4 p-4">
+			<h2 class="text-4xl mb-2 bg-primary-800 text-gray-50 inline-block p-2">Military</h2>
+			<p class="bg-primary-800 text-gray-50 p-2">A wide range of products supporting our troops from Communications to Defense.</p>
 		</div>
 	</SwiperSlide>
 	<SwiperSlide>
@@ -29,8 +29,8 @@
 			src="slider/pts-plane-2k.jpg"
 			/>
 		<div class="slide-content absolute bottom-8 left-4 p-4">
-			<h2 class="text-4xl mb-2 bg-primary-800 text-gray-50 inline-block">Commercial</h2>
-			<p class="bg-primary-800 text-gray-50">Products ranging from reference designs in the consumer market, through commercial avionics.</p>
+			<h2 class="text-4xl mb-2 bg-primary-800 text-gray-50 inline-block p-2">Commercial</h2>
+			<p class="bg-primary-800 text-gray-50 p-2">Products ranging from reference designs in the consumer market, through commercial avionics.</p>
 		</div>
 	</SwiperSlide>
 	<SwiperSlide>
@@ -38,9 +38,9 @@
 			alt="space"
 			src="slider/pts-satellite-2k.jpg"
 		/>
-		<div class="slide-content absolute bottom-0 left-0 p-4 bg-primary-800 text-gray-100">
-			<h2 class="text-2xl mb-2">Space</h2>
-			<p>Premier works with OEM Design Engineers on a wide array of products supporting the Space Markets.</p>
+		<div class="slide-content absolute bottom-8 left-4 p-4">
+			<h2 class="text-4xl mb-2 bg-primary-800 text-gray-50 inline-block p-2">Space</h2>
+			<p class="bg-primary-800 text-gray-50 p-2">Premier works with OEM Design Engineers on a wide array of products supporting the Space Markets.</p>
 		</div>
 	</SwiperSlide>
 </Swiper>
