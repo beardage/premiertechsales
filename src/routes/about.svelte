985b4703ba06f95b55da140e94ef82706af6ac4e
beardage <xypress@gmail.com>
2022-05-28 16:01:05 -0700

    refactor: updating about-us content

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -15,16 +15,14 @@
 	About Premier Technical Sales
 </h1>
 <p>
-	Premier Technical Sales is one of the leading technical Field Representatives in the South West.
-	Originally founded under Youngewirth and Olenick, we have been servicing Commercial, Medical,
-	Military and Space customers in the AZ/NM territory for over 21 years. As you will see, the
-	companies we represent are all complimentary with each other, which is one of the keys to our
-	success. This allows for incredible lead generation, as well as being laser focused on the
-	entire system block diagram and not just one portion. We pride ourselves on not only having in
-	depth knowledge of the products we represent, but our customer needs as well. Premier works with
-	our Principals and OEM Engineers from conceptual designs through production. We have worked very
-	hard to build up the reputation of being one of the best in our fields and thus how we came up
-	with the name PREMIER.
+	Premier Technical Sales is one of the leading Technical Field Representatives in the Southwest.
+	We have been servicing the Commercial, Military, Space and Medical markets for over 25 years. As
+	you will see, the companies we represent synergize with each other, which is one of the
+	foundations to our success. This allows for incredible lead generation, as well as being laser
+	focused on the entire system block diagram. We pride ourselves on having an in-depth knowledge
+	of the products as well as our customers needs. Premier works with our Principals and OEM
+	Engineers from conceptual designs through production. We have worked very hard to build up the
+	reputation of being one of the best in our field and thus how we landed on the name Premier.
 </p>
 
 <div
@@ -42,9 +40,9 @@
 			(IMC) as a Engineer Technician. After three years, I moved into Sales Manager roll (with
 			IMC) and managed all of the Reps nation-wide until 1990. I was then recruited by Piezo
 			Technology to be their Director of Sales for the Western Region, where again I managed
-			Reps for an additional 4 years and successfully grew our business. Opened Y&O AZ
-			(covering AZ/NM) in 1994 to Jan 1st of 2016. I have now purchased the AZ/NM territory
-			from Y&O, while still maintaining the same list of Principals.
+			Reps for an additional 4 years and successfully grew our business. Opened Youngworth &
+			Olenick AZ (covering AZ/NM) in 1994 to Jan 1st of 2016. I have now purchased the AZ/NM
+			territory from Y&O, while still maintaining the same list of Principals.
 		</p>
 	</div>
 </div>
@@ -53,13 +51,13 @@
 	class="container mx-auto mt-8 md:mt-16 px-8 md:px-16 py-8 md:flex md:justify-center md:items-center"
 >
 	<div class="md:w-3/5 p-8 text-left">
-		<h2 class="text-2xl font-headline mb-4">Amanda Rockrich</h2>
+		<h2 class="text-2xl font-headline mb-4">Amanda Rockrich, <em>Inside Sales Manager</em></h2>
 		<p>
-			I have spent 9 years doing customer service related work in a broad capacity. I started
-			going to school for Nursing, then quickly realized that’s not what I wanted to pursue. I
-			then met Jeff and was very interested in hearing what he did for work. I am now proud to
-			say I work for Jeff Beard and I’m excited to continue my professional career working for
-			Premier Technical Sales extending my knowledge and growth in the field.
+			I have spent 10 years in customer service and I am proud to say that I work for Premier
+			Technical Sales as the Inside Sales Manager. We are dedicated to working with the
+			Principals and Customers in all aspects of the Design and Sales process. We are known
+			for helping our customers through both times of growth and change. I look forward to
+			managing our existing relationships as well as growing new ones.
 		</p>
 	</div>
 	<div class="md:w-2/5 p-8">
