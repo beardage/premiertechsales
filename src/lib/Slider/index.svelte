e19a9614d0d3868c78783c6e90ee26afeb4eb8ab
beardage <xypress@gmail.com>
2022-05-31 13:16:28 -0700

    fix: font sizes on mobile

--- src/lib/Slider/index.svelte
+++ src/lib/Slider/index.svelte
@@ -11,11 +11,13 @@
 	<SwiperSlide>
 		<img alt="military" src="slider/pts-apache-2k.jpg" />
 		<div
-			class="slide-content absolute bottom-8 left-0 right-0 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 "
+			class="slide-content absolute bottom-8 left-0 right-0 w-full max-w-7xl mx-auto px-12 lg:px-8 "
 		>
-			<h2 class="text-4xl mb-2 bg-primary-800 text-gray-50 inline-block p-2">Military</h2>
+			<h2 class="text-xl md:text-4xl mb-2 bg-primary-800 text-gray-50 inline-block p-2">
+				Military
+			</h2>
 			<br />
-			<p class="inline-block bg-primary-800 text-gray-50 p-2">
+			<p class="text-xs md:text-base inline-block bg-primary-800 text-gray-50 p-2">
 				A wide range of products supporting our troops from Communications to Defense.
 			</p>
 		</div>
@@ -23,11 +25,13 @@
 	<SwiperSlide>
 		<img alt="commercial" src="slider/pts-plane-2k.jpg" />
 		<div
-			class="slide-content absolute bottom-8 left-0 right-0 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 "
+			class="slide-content absolute bottom-8 left-0 right-0 w-full max-w-7xl mx-auto px-12 lg:px-8 "
 		>
-			<h2 class="text-4xl mb-2 bg-primary-800 text-gray-50 inline-block p-2">Commercial</h2>
+			<h2 class="text-xl md:text-4xl mb-2 bg-primary-800 text-gray-50 inline-block p-2">
+				Commercial
+			</h2>
 			<br />
-			<p class="inline-block bg-primary-800 text-gray-50 p-2">
+			<p class="text-xs md:text-base inline-block bg-primary-800 text-gray-50 p-2">
 				Products ranging from reference designs in the consumer market, through commercial
 				avionics.
 			</p>
@@ -36,11 +40,13 @@
 	<SwiperSlide>
 		<img alt="space" src="slider/pts-satellite-2k.jpg" />
 		<div
-			class="slide-content absolute bottom-8 left-0 right-0 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"
+			class="slide-content absolute bottom-8 left-0 right-0 w-full max-w-7xl mx-auto px-12 lg:px-8"
 		>
-			<h2 class="text-4xl mb-2 bg-primary-800 text-gray-50 inline-block p-2">Space</h2>
+			<h2 class="text-xl md:text-4xl mb-2 bg-primary-800 text-gray-50 inline-block p-2">
+				Space
+			</h2>
 			<br />
-			<p class="inline-block bg-primary-800 text-gray-50 p-2">
+			<p class="text-xs md:text-base inline-block bg-primary-800 text-gray-50 p-2">
 				Premier works with OEM Design Engineers on a wide array of products supporting the
 				Space Markets.
 			</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,12 +11,12 @@
 	/>
 </svelte:head>
 
-<div class="w-full">
+<div class="w-full mt-20 md:mt-0">
 	<Slider />
 </div>
 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 my-8">
-	<h1 class="text-3xl mt-8 mb-4 inline-block">Premier Technical Sales</h1>
-	<p>
+	<h1 class="text-xl md:text-3xl mt-8 mb-4 inline-block">Premier Technical Sales</h1>
+	<p class="text-sm md:text-base">
 		Premier Tech Sales is a complete solution to your Field Representative needs in the states
 		of Arizona and New Mexico. We are focused on RF/Microwave and Digital products and take
 		great pride in offering professional services, working with our customers to support both
