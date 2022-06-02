7d76b4d7a1e5fc7b71cba51301980a8bc8982a58
beardage <xypress@gmail.com>
2022-06-02 00:03:12 -0700

    feature: adding headlessui and using transition component for mobile nav

--- src/lib/Header/MobileNavigation.svelte
+++ src/lib/Header/MobileNavigation.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Transition } from "@rgossiaux/svelte-headlessui";
 	import { createEventDispatcher } from 'svelte';
 
 	export let open: boolean;
@@ -9,10 +10,16 @@
 	}
 </script>
 
-<div
-	class:translate-x-0={open}
-	class:translate-x-full={!open}
-	class="fixed top-0 bottom-0 right-0 w-3/4 bg-gray-50 z-[100] shadow-2xl transition-transform duration-200 ease-in-out"
+<Transition
+	show={open}
+	enter="transition-translate duration-200"
+	enterFrom="translate-x-full"
+	enterTo="translate-x-0"
+	leave="transition-translate duration-200"
+	leaveFrom="opacity-x-0"
+	leaveTo="opacity-x-full"
 >
-	<button on:click={closeNavigation}>x</button>
-</div>
+	<div class="fixed md:hidden top-0 bottom-0 right-0 w-3/4 bg-gray-50 z-[100] shadow-2xl">
+		<button on:click={closeNavigation}>x</button>
+	</div>
+</Transition>
