7e99a2ce91f6b62f93b9241af951179964fe9123
beardage <xypress@gmail.com>
2022-06-04 13:39:23 -0700

    feature: adding dark variant logo to mobile navigation

--- src/lib/Header/MobileNavigation.svelte
+++ src/lib/Header/MobileNavigation.svelte
@@ -12,7 +12,6 @@
 </script>
 
 <div class:pointer-events-auto={open} class:pointer-events-none={!open} class="overflow-hidden fixed top-0 right-0 bottom-0 left-0 z-[100]">
-		
 	<Transition
 		show={open}
 		enter="transition ease-in-out duration-250 transform"
@@ -22,10 +21,15 @@
 			leaveFrom="translate-x-0"
 			leaveTo="translate-x-full"
 	>
-		<div class="fixed top-0 right-0 h-screen w-3/4 bg-gray-50 shadow-2xl z-[100]">
-			<button on:click={closeNavigation}>
+		<div class="fixed top-0 right-0 h-screen w-3/4 bg-gray-50 shadow-2xl z-[100]">		
+			<button class="absolute -left-6 top-2" on:click={closeNavigation}>
 				<img class="w-5" src="times-solid.svg" alt="close menu"/>
 			</button>
+			<div class="border-b">
+				<img class="w-3/5 m-auto py-4" src="pts-logo-dark.svg" alt="Premier Technical Sales Dark Logo"/>
+			</div>
+
+
 		</div>
 	</Transition>
 </div>
