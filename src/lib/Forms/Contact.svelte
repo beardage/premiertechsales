08fbfd35e28c1d57d24dcd3fc060e0ce22c4e66f
beardage <xypress@gmail.com>
2022-06-04 22:32:32 -0700

    fix: final attempt for the night

--- src/lib/Forms/Contact.svelte
+++ src/lib/Forms/Contact.svelte
@@ -9,7 +9,7 @@
 		fetch("/", {
 			method: "POST",
 			headers: { "Content-Type": "application/x-www-form-urlencoded" },
-			body: formData
+			body: new URLSearchParams(formData).toString(),
 		})
 			.then(() => showSuccessMessage())
 			.catch((error) => console.log(error));
